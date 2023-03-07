dd960b40ee6b1dc0b7e25434ffe2e13baa04e5a8
Emmanuel Okobi <ebokobi@aggies.ncat.edu>
2023-03-07 23:35:13 +0000

    try

--- src/main/java/ArraysAreEqual.java
+++ src/main/java/ArraysAreEqual.java
@@ -18,10 +18,8 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
-        for(int i : a) {
-            if(a.equals(b)) {
-                return true;
-            }
+        if(a.equals(b)) {
+            return true;
         }
         return false;
     }
